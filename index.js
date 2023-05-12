e875349cdf1c6d24ccee5b0fb6a7b81834437bd1
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-12 20:31:14 +0200

    Make exercise 2 async

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ const getRandomPokemon = async () => {
       console.log(`ERROR: ${error.stack}`);
   }
 }
+
+/* Ejercicio 2.- Declara una funcion getImageAndName que retorne el nombre y la URL de la imagen de un pokemon => (return {img, name}) */ 
+const getImageAndName = async () => {
+  try {
+    let idNum = Math.floor(Math.random()*135)
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idNum}/`);
+    let data = await response.json()
+    let img = data.sprites.front_default;
+    let name = data.name;
+    const obj = {img, name}
+    return obj;
+  }
+  catch (error) {
+      console.log(`ERROR: ${error.stack}`);
+  }
+}
